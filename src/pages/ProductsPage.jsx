ff353cc764739ca6ad445f078b4df229400cbc8b
Jorge L. <jorge.lopez@newfold.com>
2025-01-05 19:22:18 -0400

    products

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,4 +1,4 @@
-import { Box, Grid, TextField } from '@mui/material';
+import { Box, Chip, Grid, TextField } from '@mui/material';
 import { useEffect, useState } from 'react';
 import NavigationButton from '../components/navigation-button';
 import PageContainer from '../components/PageContainer';
@@ -11,6 +11,7 @@ import {
 } from '../helpers/cartHelpers';
 import { useLocation } from 'react-router-dom';
 import { useAppContext } from '../context/AppContext';
+import { getRandomColorFromString } from '../utils/colors';
 
 const ProductPage = () => {
   const [idsInCart, setIdsInCart] = useState([]);
@@ -21,6 +22,7 @@ const ProductPage = () => {
 
   const location = useLocation();
   const { isAuthenticated } = useAppContext();
+  const MOSTRAR_TODOS = 'Todos';
 
   useEffect(() => {
     const fetchData = async () => {
@@ -79,8 +81,12 @@ const ProductPage = () => {
     filterProducts(category, arrProducts);
   }, [location.search]);
 
-  const filterProducts = (searchTerm, arrProducts) => {
-    let filtered = arrProducts;
+  const filterProducts = (searchTerm, arrProducts, category = null) => {
+    let filtered = arrProducts.filter((product) =>
+      category && category != MOSTRAR_TODOS
+        ? product.category === category
+        : true
+    );
 
     if (searchTerm) {
       filtered = filtered.filter((product) =>
@@ -95,6 +101,44 @@ const ProductPage = () => {
     const value = e.target.value;
     setSearch(value);
     filterProducts(value, arrProducts);
+  };
+
+  const handleCategoryFilter = (category, searchTerm) => {
+    filterProducts(searchTerm, arrProducts, category);
+  };
+
+  const generateCategories = () => {
+    const arr = arrProducts.map((product) => product.category);
+    const uniqueCategories = [...new Set(arr)];
+
+    return (
+      <Box
+        sx={{
+          display: 'flex',
+          gap: 4,
+          margin: '1rem 0',
+          background: '#ffffff',
+          padding: '1rem',
+          boxSizing: 'border-box',
+          justifyContent: 'center',
+          flexWrap: 'wrap',
+        }}
+      >
+        <Chip
+          sx={{ background: 'white', outline: 'solid 1px #ccc' }}
+          label={MOSTRAR_TODOS}
+          onClick={() => handleCategoryFilter(MOSTRAR_TODOS, search)}
+        />
+        {uniqueCategories.map((category) => (
+          <Chip
+            sx={{ background: getRandomColorFromString(category, search) }}
+            key={category}
+            label={category}
+            onClick={() => handleCategoryFilter(category, search)}
+          />
+        ))}
+      </Box>
+    );
   };
 
   return (
@@ -113,7 +157,8 @@ const ProductPage = () => {
         <Box
           sx={{
             margin: '1rem  auto 4rem',
-            maxWidth: '400px',
+            width: { xs: '350px', md: '400px' },
+            maxWidth: { xs: '100%', md: '400px' },
             background: 'white',
           }}
         >
@@ -128,10 +173,19 @@ const ProductPage = () => {
         </Box>
       )}
 
+      {/* Categories */}
+      {generateCategories()}
+
       {/* products container */}
       <Box
         id="product-cards-container"
-        sx={{ display: 'flex', flexWrap: 'wrap', gap: 2, minHeight: '500px' }}
+        sx={{
+          display: 'flex',
+          flexWrap: 'wrap',
+          gap: 4,
+          minHeight: '500px',
+          justifyContent: 'center',
+        }}
       >
         {!isLoading &&
           filteredProducts.map((product) => {
